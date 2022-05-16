8ae511b64f6678fcdf92684b75be884eac8e8b3c
Nathan-Thibault <nathan.thibault@utt.fr>
2022-05-16 15:20:56 +0200

    start implementing title screen

--- src/fr/utt/eg23/labattailledesprogrammes/LaBattailleDesProgrammes.java
+++ src/fr/utt/eg23/labattailledesprogrammes/LaBattailleDesProgrammes.java
@@ -1,12 +1,25 @@
 package fr.utt.eg23.labattailledesprogrammes;
 
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 public class LaBattailleDesProgrammes {
 
 	private JFrame frame;
+	
+	public final static Color COLOR_BACKGROUND = new Color(55, 60, 72);
 
 	/**
 	 * Launch the application.
@@ -36,8 +49,26 @@ public class LaBattailleDesProgrammes {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setTitle("La Bataille des Programmes");
+		frame.setLocationRelativeTo(null);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		//frame.setUndecorated(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel labelImageTitre = new JLabel("");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		labelImageTitre.setIcon(getImageToSize("ecranTitre.jpg", (int) screenSize.getWidth(), (int) screenSize.getHeight()));
+		frame.getContentPane().add(labelImageTitre, BorderLayout.CENTER);
 	}
-
+	
+	public ImageIcon getImageToSize(String imageName, int width, int height) {
+		try {
+			BufferedImage buffImg = ImageIO.read(LaBattailleDesProgrammes.class.getResource("/images/" + imageName));
+			Image img = buffImg.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+			return new ImageIcon(img);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
