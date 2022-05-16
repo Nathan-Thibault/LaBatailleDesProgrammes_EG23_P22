921672ad144abb61f6e95deaf6db5d4cbb6beae6
Nathan-Thibault <nathan.thibault@utt.fr>
2022-05-16 14:39:54 +0200

    Initialize project

---
+++ src/fr/utt/eg23/labattailledesprogrammes/LaBattailleDesProgrammes.java
@@ -0,0 +1,43 @@
+package fr.utt.eg23.labattailledesprogrammes;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class LaBattailleDesProgrammes {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LaBattailleDesProgrammes window = new LaBattailleDesProgrammes();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LaBattailleDesProgrammes() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
