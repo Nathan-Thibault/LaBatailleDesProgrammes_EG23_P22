7f793549f114b2f8828568d5901e7d9d5fc206c9
Nathan-Thibault <nathan.thibault@utt.fr>
2022-05-27 15:40:51 +0200

    add enums for branches and fighter types

---
+++ src/fr/utt/eg23/labattailledesprogrammes/FighterType.java
@@ -0,0 +1,32 @@
+package fr.utt.eg23.labattailledesprogrammes;
+
+import java.awt.Color;
+
+public enum FighterType {
+    SOLDIER("Soldat", "so", new Color(9, 9, 10)),
+    ELITE_SOLDIER("Soldat d'élite", "se", new Color(100, 100, 101)),
+    MASTER_OF_WAR("Maître de guerre", "mg", new Color(237, 231, 25));
+
+    private final String name;
+    private final String fileNameAffix;
+    private final Color color;
+
+    FighterType(String name, String fileNameAffix, Color color) {
+        this.name = name;
+        this.fileNameAffix = fileNameAffix;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getFileNameAffix() {
+        return fileNameAffix;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
---
+++ src/fr/utt/eg23/labattailledesprogrammes/UTTBranch.java
@@ -0,0 +1,28 @@
+package fr.utt.eg23.labattailledesprogrammes;
+
+public enum UTTBranch {
+    ISI("Informatique et Système d'Information", "isi"),
+    RT("Réseaux et Télécomunications", "rt"),
+    A2I("Automatique et Informatique Industrielle", "a2i"),
+    GM("Génie Mécanique", "gm"),
+    MTE("Matériaux : Technologie et Économie", "mte"),
+    MM("Matériaux et Mécanique", "mm"),
+    GI("Génie Industriel", "gi");
+
+    private final String name;
+    private final String fileNameAffix;
+
+    UTTBranch(String name, String fileNameAffix) {
+        this.name = name;
+        this.fileNameAffix = fileNameAffix;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getFileNameAffix() {
+        return fileNameAffix;
+    }
+}
